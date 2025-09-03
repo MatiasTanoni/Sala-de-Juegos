7386498c491d17afa2c1eec0aeac5154641fd88b
Matias Tanoni <matitanoni@gmail.com>
2025-09-03 18:18:38 -0300

    spinner component

--- TP-1/src/app/pages/about/about.ts
+++ TP-1/src/app/pages/about/about.ts
@@ -2,18 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import { GithubService } from '../../services/github';
 import { Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
+import { Spinner } from '../../components/spinner/spinner';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.html',
   standalone: true,
-  imports: [AsyncPipe], 
+  imports: [AsyncPipe, Spinner],
 })
 export class About implements OnInit {
   username = 'MatiasTanoni';
   userData$!: Observable<any>;
 
-  constructor(private githubService: GithubService) {}
+  constructor(private githubService: GithubService) { }
 
   ngOnInit(): void {
     this.userData$ = this.githubService.getUser(this.username);
