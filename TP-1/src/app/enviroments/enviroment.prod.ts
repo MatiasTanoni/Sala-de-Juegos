35bffca35083fa7217f2d93568a3b5c8d6842ea5
MatiasTanoni <matitanoni@gmail.com>
2025-09-04 17:55:35 -0300

    api keys

--- TP-1/src/app/enviroments/enviroment.prod.ts
+++ TP-1/src/app/enviroments/enviroment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    supabaseUrl: 'https://dgnvjlzhaoxhaftpdurq.supabase.co/',
-    supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRnbnZqbHpoYW94aGFmdHBkdXJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ1NTUxNTEsImV4cCI6MjA2MDEzMTE1MX0.yjRBf1UdsEqWm8YBbB7NSXYtVqgLV_J65TTDvR_DWsQ'
+    supabaseUrl: 'https://uvqqbgnanwjlwyhrkvhc.supabase.co',
+    supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2cXFiZ25hbndqbHd5aHJrdmhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY3NjkwNTQsImV4cCI6MjA3MjM0NTA1NH0.w4_0L-gJRFV4CPQ6H7UxDv3uleC9ufb-yk7EXopKo2w'
 };
