2bd12f7bc4b604bc8ece9660bf83c5adcfeff6b2
MatiasTanoni <matitanoni@gmail.com>
2025-09-15 22:56:58 -0300

    chat terminado

--- TP-1/src/app/pages/chat/chat.ts
+++ TP-1/src/app/pages/chat/chat.ts
@@ -1,18 +1,35 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import Mensaje from '../../interfaces/mensaje';
 import { Realtime } from '../../services/realtime/realtime';
+import { Auth } from '../../services/auth/auth';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-chat',
-  imports: [FormsModule],
+  imports: [FormsModule, DatePipe],
   templateUrl: './chat.html',
   styleUrl: './chat.css'
 })
 export class Chat {
   realtime = inject(Realtime);
+  auth = inject(Auth);
 
   mensajes = signal<Mensaje[]>([]);
+  msj = '';
+
+  usuario: string | null = null;
+
+  constructor() {
+    effect(() => {
+      const u = this.auth.user();
+      if (u && typeof u !== 'boolean') {
+        this.usuario = u.name;
+      } else {
+        this.usuario = null;
+      }
+    });
+  }
 
   ngOnInit(): void {
     this.realtime.traerTodosFijo().then((array) => {
@@ -22,11 +39,7 @@ export class Chat {
     this.realtime.canal
       .on(
         'postgres_changes',
-        {
-          event: '*',
-          schema: 'public',
-          table: 'chat',
-        },
+        { event: '*', schema: 'public', table: 'chat' },
         (payload) => {
           this.mensajes.update((valorAnterior) => {
             valorAnterior.push(payload.new as Mensaje);
@@ -37,10 +50,9 @@ export class Chat {
       .subscribe();
   }
 
-  msj = '';
-  usuario = '';
-
   enviarMensaje() {
+    if (!this.usuario || !this.msj.trim()) return;
     this.realtime.crear(this.msj, this.usuario);
+    this.msj = '';
   }
 }
