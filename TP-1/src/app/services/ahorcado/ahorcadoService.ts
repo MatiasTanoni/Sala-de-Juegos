72cadc6fb843de8df8c30b4a4cdab7ca0300fb43
MatiasTanoni <matitanoni@gmail.com>
2025-09-27 00:21:25 -0300

    cargar nueva palabra y q sea random 2 funciones

--- TP-1/src/app/services/ahorcado/ahorcadoService.ts
+++ TP-1/src/app/services/ahorcado/ahorcadoService.ts
@@ -6,7 +6,14 @@ import { Game } from '../../models/game/game';
 })
 export class AhorcadoService extends Game {
   private name: string = "ahorcado";
+  private word: string = '';
+  private displayed: string[] = [];
   private guessedLetters: Set<string> = new Set();
+  private allWords = [
+    'ANGULAR', 'PROGRAMAR', 'DESARROLLO', 'AHORCADO', 'PREGUNTAS',
+    'JAVASCRIPT', 'VARIABLE', 'COMPONENTE', 'FUNCION', 'SERVICIO',
+    'MODULO', 'TYPESCRIPT', 'TEMPLATE', 'HTML', 'CSS', 'JUEGO', 'LOGICA'
+  ];
 
   newGame() {
     this.setLives(7);
@@ -18,12 +25,31 @@ export class AhorcadoService extends Game {
     this.startTimer(() => {
       this.endGame(this.victory, this.name);
     });
+
+    this.loadNewWord();
+
   }
 
+  private getRandomWord(): string {
+    const remainingWords = this.wordList.filter(word => !this.guessedWords.has(word));
+    const index = Math.floor(Math.random() * remainingWords.length);
+    return remainingWords[index];
+  }
+
+  private loadNewWord(): void {
+    const remainingWords = this.wordList.filter(word => !this.guessedWords.has(word));
+    if (remainingWords.length === 0) {
+      this.endGame(this.victory, this.name);
+      return;
+    }
+    this.word = this.getRandomWord();
+    this.displayed = Array(this.word.length).fill('_');
+    this.guessedLetters.clear();
+  }
   isGameOver(): boolean {
     return this.getLives() === 0;
   }
-  
+
   guessLetter(letter: string) {
 
   }
