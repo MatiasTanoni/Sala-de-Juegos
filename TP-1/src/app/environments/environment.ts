5cb477453a5f4937987b4d82c4192b2acc906ec2
MatiasTanoni <matitanoni@gmail.com>
2026-03-05 15:13:50 -0300

    cambiamos enviroment

--- TP-1/src/app/environments/environment.ts
+++ TP-1/src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    supabaseUrl: 'https://uvqqbgnanwjlwyhrkvhc.supabase.co',
-    supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2cXFiZ25hbndqbHd5aHJrdmhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY3NjkwNTQsImV4cCI6MjA3MjM0NTA1NH0.w4_0L-gJRFV4CPQ6H7UxDv3uleC9ufb-yk7EXopKo2w'
+    supabaseUrl: 'https://yzezhqacefkvesqwsnii.supabase.co',
+    supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6ZXpocWFjZWZrdmVzcXdzbmlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3MjgxOTIsImV4cCI6MjA4ODMwNDE5Mn0.3GFRng6dOTNI8L_pq7qJNPCMzaFp3k6KJMAIksHEb6s'
 };
